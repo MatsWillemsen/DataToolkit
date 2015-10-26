0e553feedeb59b15497df3ba066ca3970b52771b
Mats Willemsen <mats.willemsen@gmail.com>
2015-10-26 14:00:30 +0100

    Opdracht 1 inleveren
    Signed-off-by: Mats Willemsen <mats.willemsen@gmail.com>

---
+++ Homework/Scraping/tvscraper.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# Name: Mats Willemsen
+# Student number: 10247556
+'''
+This script scrapes IMDB and outputs a CSV file with highest ranking tv series.
+'''
+# IF YOU WANT TO TEST YOUR ATTEMPT, RUN THE test-tvscraper.py SCRIPT.
+import csv
+import codecs
+from pattern.web import URL, DOM
+
+TARGET_URL = "http://www.imdb.com/search/title?num_votes=5000,&sort=user_rating,desc&start=1&title_type=tv_series"
+BACKUP_HTML = 'tvseries.html'
+OUTPUT_CSV = 'tvseries.csv'
+
+
+def extract_tvseries(dom):
+    '''
+    Extract a list of highest ranking TV series from DOM (of IMDB page).
+
+    Each TV series entry should contain the following fields:
+    - TV Title
+    - Ranking
+    - Genres (comma separated if more than one)
+    - Actors/actresses (comma separated if more than one)
+    - Runtime (only a number!)
+    '''
+    ret = []
+    shows = dom('.detailed > .title')
+    for show in shows:
+        title = show('a')[0].content
+        ranking = show('.rating-rating .value')[0].content
+        genres = ",".join([x.content for x in show('.genre > a')])
+        actors = ",".join([x.content for x in show('.credit > a')])
+        runtime = int(show('.runtime')[0].content.replace(" mins.",""))
+        ret.append([title, ranking, genres, actors, runtime])
+    # ADD YOUR CODE HERE TO EXTRACT THE ABOVE INFORMATION ABOUT THE
+    # HIGHEST RANKING TV-SERIES
+    # NOTE: FOR THIS EXERCISE YOU ARE ALLOWED (BUT NOT REQUIRED) TO IGNORE
+    # UNICODE CHARACTERS AND SIMPLY LEAVE THEM OUT OF THE OUTPUT.
+
+    return ret  # replace this line as well as appropriate
+
+
+def save_csv(f, tvseries):
+    '''
+    Output a CSV file containing highest ranking TV-series.
+    '''
+    writer = csv.writer(f)
+    writer.writerow(['Title', 'Ranking', 'Genre', 'Actors', 'Runtime'])
+    for x in tvseries:
+        writer.writerow([unicode(y).encode("utf-8") for y in x])
+    # ADD SOME CODE OF YOURSELF HERE TO WRITE THE TV-SERIES TO DISK
+
+if __name__ == '__main__':
+    # Download the HTML file
+    url = URL(TARGET_URL)
+    html = url.download()
+
+    # Save a copy to disk in the current directory, this serves as an backup
+    # of the original HTML, will be used in testing / grading.
+    with open(BACKUP_HTML, 'wb') as f:
+        f.write(html)
+
+    # Parse the HTML file into a DOM representation
+    dom = DOM(html)
+
+    # Extract the tv series (using the function you implemented)
+    tvseries = extract_tvseries(dom)
+
+    # Write the CSV file to disk (including a header)
+    with open(OUTPUT_CSV, 'wb') as output_file:
+        save_csv(output_file, tvseries)
